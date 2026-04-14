01a99bd19cc55d9dd245160651289cd7a135260c
CielSsongsSeta <gch00536@naver.com>
2026-04-14 21:16:57 +0900

    LLM 데이터셋 수정하여 game_data.duckdb 생성

--- 1_code/dbt_LLM/dbt_LLM/models/llm/llm_train_set.sql
+++ 1_code/dbt_LLM/dbt_LLM/models/llm/llm_train_set.sql
@@ -1,8 +1,13 @@
 -- 3. 질문 - 맥락 - 답변 구조로 생성
 SELECT
     '게임 유저의 조작 피로도와 결제 심리를 분석하세요.' as instruction,
-    s.selftext as input, -- stg_comments의 원본 텍스트 사용
-    '이 유저는 현재 ' || i.user_behavior_type || ' 상태입니다. ' ||
-    '분석 결과: ' || s.category || ' 지표가 높으며 감성은 ' || i.sentiment || '입니다.' as output
-FROM {{ ref('int_fatigue_bm_link') }} i
-JOIN {{ ref('stg_comments') }} s USING (id)
+    selftext as input,
+    '분석 결과: ' || category || ' 지표가 감지되었습니다. ' ||
+    '이 유저는 현재 ' || 
+    CASE 
+        WHEN is_bm_talk = 1 AND is_high_ccs = 1 THEN '과금 유도 및 조작 피로도 높음'
+        WHEN is_bm_talk = 1 THEN '과금 피로도 높음'
+        WHEN is_high_ccs = 1 THEN '조작 피로도 높음'
+        ELSE '일반 유저'
+    END || ' 상태입니다.' as output
+FROM {{ ref('stg_comments') }}
